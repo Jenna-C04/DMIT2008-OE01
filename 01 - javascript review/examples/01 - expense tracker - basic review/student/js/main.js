7161a451111f519e47de253a99e10d6fb9699c18
Jenna <jennacoates04@gmail.com>
2026-05-14 18:25:35 -0600

    add - set up DOM object and data import for expense cards

--- 01 - javascript review/examples/01 - expense tracker - basic review/student/js/main.js
+++ 01 - javascript review/examples/01 - expense tracker - basic review/student/js/main.js
@@ -1,1 +1,14 @@
-//Instruction On expense-tracker-code-explained.md
+// Importing data from the file (in lieu of a database or API)
+import expenses from "./expense-data.js";
+
+// Grab relevant DOM elements
+const expenseContainer = document.getElementById('expense-container')
+
+// Render data
+function renderExpenses(expenseData) {
+    //...
+    
+}
+
+// Call function to render
+renderExpenses(expenses);
