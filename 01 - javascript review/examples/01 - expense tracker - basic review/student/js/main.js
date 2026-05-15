f014da42f5f8b37a8151d7cf72fd9db7ab3926a1
Jenna <jennacoates04@gmail.com>
2026-05-14 18:45:37 -0600

    add - Render out HTML cards

--- 01 - javascript review/examples/01 - expense tracker - basic review/student/js/main.js
+++ 01 - javascript review/examples/01 - expense tracker - basic review/student/js/main.js
@@ -2,11 +2,36 @@
 import expenses from "./expense-data.js";
 
 // Grab relevant DOM elements
-const expenseContainer = document.getElementById('expense-container')
+const expenseContainer = document.getElementById('expense-container');
+const searchTerm = document.getElementById('searchbox');
 
 // Render data
 function renderExpenses(expenseData) {
-    //...
+    // First, Clear out existing HTML for the container, cause were about to re-render it
+    expenseContainer.innerHTML = "";
+
+    // then, take our array of data, then render a card for each
+    expenseData.forEach(
+        (expense) => {
+            expenseContainer.innerHTML += 
+            `
+                <div class="card" id="${expense.id}">
+                    <div class="header">
+                        <div>
+                            <div class="title">${expense.title}</div>
+                            <div class="meta category">${expense.category}</div>
+                        </div>
+                        <div class="amount">${expense.amount}</div>
+                    </div>
+                    <div class="meta date">${expense.date}</div>
+                    <div class="actions">
+                        <button class="edit-btn" id="${expense.id}">Edit</button>
+                        <button class="delete-btn" id="${expense.id}>Delete</button>
+                    </div>
+                </div>
+            `
+        }
+    )
     
 }
 
