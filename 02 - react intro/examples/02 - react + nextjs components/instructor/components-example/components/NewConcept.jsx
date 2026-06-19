cbde204c3d8d6ebbd48adad9e7ab66bf4c5cfcc9
oa <oliver+github@abolish.golf>
2026-06-18 18:44:18 -0600

    rev [02 - react intro]: ex02 - IMO, use destructuring instead for props

--- 02 - react intro/examples/02 - react + nextjs components/instructor/components-example/components/NewConcept.jsx
+++ 02 - react intro/examples/02 - react + nextjs components/instructor/components-example/components/NewConcept.jsx
@@ -7,8 +7,12 @@
 // You'd use this in JSX as:
 //   <NewConcept concept="how to make a component" author="Oliver" />
 
-export default function NewConcept(props) {
+export default function NewConcept({ concept }) {
+  // I think destructuring input props is the better way, because then it's
+  // immediately apparent what you're ingesting/using. If we didn't destructure
+  // and just had one object/argument for all props, like before, we'd have to read
+  // allllllll the code to understand which props were being used how first.
   return (
-    <p>In this class, we've learned: {props.concept}</p>
+    <p>In this class, we've learned: {concept}</p>
   )
 }
