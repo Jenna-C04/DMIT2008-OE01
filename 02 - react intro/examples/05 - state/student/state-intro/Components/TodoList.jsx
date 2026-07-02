0954d47e848f230b08d63cf1c814caf43ba2ae7a
Jenna <jennacoates04@gmail.com>
2026-07-02 17:52:38 -0600

    write 'todo item' into the array - updating arrays with state

--- 02 - react intro/examples/05 - state/student/state-intro/Components/TodoList.jsx
+++ 02 - react intro/examples/05 - state/student/state-intro/Components/TodoList.jsx
@@ -4,7 +4,11 @@
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+
 import { useState } from "react";
+
 
 export default function TodoList() {
 
@@ -12,34 +16,44 @@ export default function TodoList() {
     // second term: the setter function; the only thing allowed to change that value
     // initial param for useState: initial value
 
-    const [todoItem, setTodoItem] = useState("temp")
+    const [todoItem, setTodoItem] = useState("")
+    const [allTodos, setAllTodos] = useState([])
 
     const onTodoTextChange = (event) => {
         setTodoItem(event.tartget.value)
     }
 
     const onAddTodoClick = () => {
-        console.log("clicked")
+        console.log("clicked!")
+        // we can't just e.g. push a new element to the array, because the setter is the only thing that can change the stateful variable, so i need to construct a new array
+        // Here, that just means... everything I had before plus the new items
+        const newTodos = [...allTodos, todoItem]
+        // We'll use the speread opperator (...) for that. Basically, it means taking all the elements if the array without actually *being* in that container
+        setAllTodos(newTodos)
     }
 
     return (
-        <>
-            <TextField id="standard-basic"
-            label="New todo item"
-            variant="standard"
-            sx={{ width:'75%'}}
-            value={todoItem}
-            onChange={setTodoItem}
-            />
-            <Button 
-            variant="contained"
-            onClick={onAddTodoClick}
-            >
-                Add Todo Item
-            </Button>
-        
-        </>
+        <Grid container spacing={2}>
 
+            <Grid size={10}>
+                <TextField id="standard-basic"
+                label="New todo item"
+                variant="standard"
+                sx={{ width:'100%'}}
+                value={todoItem}
+                onChange={onTodoTextChange}
+                />
+            </Grid>
 
+            <Grid size={2}>
+                <Button 
+                variant="contained"
+                onClick={onAddTodoClick}
+                >
+                    Add Todo Item
+                </Button>
+            </Grid>
+
+        </Grid>
     )
 }
